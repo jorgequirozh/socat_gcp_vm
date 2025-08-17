27a453ae38f9594f223d6e43ededc0a07f346ab7
jorgequiroz <jorge@quirozhernandez.cl>
2025-08-17 12:25:46 -0400

    Remove firewall rules

--- networking.tf
+++ networking.tf
@@ -7,8 +7,10 @@ resource "google_compute_network" "vpc" {
 
 # create public subnet
 resource "google_compute_subnetwork" "network_subnet" {
-  name          = "socat-subnet"
-  ip_cidr_range = var.network-subnet-cidr
-  network       = google_compute_network.vpc.name
-  region        = var.gcp_region
+  name             = "socat-subnet"
+  ip_cidr_range    = var.network-subnet-cidr
+  network          = google_compute_network.vpc.name
+  region           = var.gcp_region
+  stack_type       = "IPV4_ONLY"
+#  ipv6_access_type = "EXTERNAL"
 }
